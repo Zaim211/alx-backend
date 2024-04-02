0f6fd9162ebc34c3d82e930497a9cbd981ce98b4
Zaim211 <yousseftitazaim@gmail.com>
2024-04-02 23:28:18 +0000

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -23,6 +23,6 @@ class LIFOCache(BaseCaching):
 
     def get(self, key):
         """ Return the value linked to a given key, or None """
-        if key is None or key not in self.cache_data(keys):
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
