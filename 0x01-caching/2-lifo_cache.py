8c4c5aa5f136f9f53f2bcc2dc829698c59f53158
Zaim211 <yousseftitazaim@gmail.com>
2024-04-02 23:19:46 +0000

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -17,7 +17,7 @@ class LIFOCache(BaseCaching):
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
                     and key not in self.cache_data.keys():
-                last_key = self.cache_data.popitem()
+                last_key, last_value = self.cache_data.popitem()
                 print('DISCARD: {}'.format(last_key))
         self.cache_data[key] = item
 
