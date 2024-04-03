d633e3e314942c92031e3eb4830d14bb48ff8c93
Zaim211 <yousseftitazaim@gmail.com>
2024-04-03 22:58:59 +0000

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,22 +14,23 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """ Cache a key-value pair """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            if key not in self.order:
-                self.order.append(key)
+        if key is None or item is None:
+            pass
         else:
-            self.order.append(
-                self.order.pop(self.order.index(key)))
-        length = len(self.order)
-        if length >= BaseCaching.MAX_ITEMS:
-            discarded_key = self.order.pop(0)
-            del self.cache_data[discarded_key]
-            print('DISCARD: {}'.format(discarded_key))
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print('DISCARD: {}'.format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            if key in self.order:
+                del self.order[self.order.index(key)]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Return the value linked to a given key, or Non """
         if key is not None and key in self.cache_data.keys():
-            self.order.append(self.order.pop(self.order.index(key)))
-            return self.cache_data.get(key)
+            del self.order[self.order.index(key)]
+            self.order.append(key)
+            return self.cache_data[key]
         return None
