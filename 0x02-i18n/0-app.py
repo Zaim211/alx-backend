7381884f4f839880fd0446b50c40d6141e7c0b1d
Zaim211 <yousseftitazaim@gmail.com>
2024-04-09 22:18:25 +0000

    Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Flask app
+Basic Flask app
 """
 from flask import Flask, render_template
 
@@ -10,11 +10,11 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """
-    Handles / route
+    """ Setup Flask
+    Handle / route
     """
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(port="5000", host="localhost", debug=True)
