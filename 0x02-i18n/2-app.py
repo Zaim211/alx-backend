c4e40f1679cdcd2e8090333b404affe17f058aa6
Zaim211 <yousseftitazaim@gmail.com>
2024-04-09 22:49:09 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,9 +15,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
-    """  determine the best match with our 
+    """  determine the best match with our
     supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
